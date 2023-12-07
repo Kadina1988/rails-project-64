4ad016ab2be73d9de0b7ad94f52c283e4621a54a
Kadina1988 <kadina.aziev@mail.ru>
2023-12-07 18:30:17 +0300

    added simple_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
-  def nav_panel
+  def sign_in_helper
     if user_signed_in?
       link_to 'New post', new_post_path
     else
       link_to 'Sign in', new_user_session_path, class: 'btn bnt-secondary'
+    end
+  end
+
+  def sign_out_helper
+    if user_signed_in?
+      button_to 'Sign out', destroy_user_session_path, method: :delete
+    else
+      link_to 'Sign up', new_user_registration_path
     end
   end
 end
