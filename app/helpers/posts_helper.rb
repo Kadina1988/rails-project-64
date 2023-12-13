b8b9a2a4d1e388e39d9ea9f4d818b08daa751a68
Kadina1988 <kadina.aziev@mail.ru>
2023-12-13 14:31:08 +0300

    pry

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def nest(attributes)
-    attributes.each do |attr|
-      concat(content_tag(:h4, attr.content))
-      next unless attr.children?
-
-      attr.descendants.each do |ch|
-        concat(content_tag(:p, ch.content))
-      end
-    end
-  end
 end
