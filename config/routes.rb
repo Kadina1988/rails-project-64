bb5fc46d430544d21d0cc0ff8f32bd65a9e03e95
Kadina1988 <kadina.aziev@mail.ru>
2023-12-13 23:04:28 +0300

    added ancelstry gem

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :posts, shallow: true do
-    resources :comments, only: %i[create new]
+    resources :comments, only: %i[create], as: 'post_comments'
   end
   root 'posts#index'
 end
