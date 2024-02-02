9146fde693f9e99e623e9c911262307fed7f9ddb
Kadina1988 <kadina.aziev@mail.ru>
2024-02-02 23:21:10 +0300

    z

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,7 @@ module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    # Setup all fixtu .
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
