6d25ca07c31c85e0e5bba1bce164cbc5b10f4cab
Kadina1988 <kadina.aziev@mail.ru>
2024-02-03 22:01:41 +0300

    a

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module RailsProject64
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
     # config.active_support.default_message_encryptor_serializer = :json
     # config.active_support.use_marshal_serialization = false
     # config.active_support.fallback_to_marshal_deserialization = false
