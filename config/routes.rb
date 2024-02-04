60b05a0ce906a3b1906c0465b291f23c3973d35e
Kadina1988 <kadina.aziev@mail.ru>
2024-02-04 20:51:04 +0300

    x

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :posts, shallow: true do
-    resources :comments, only: %i[create new]
+    resources :comments, only: %i[create], as: 'post_comments'
+    resources :comments, only: :new
     resources :likes, only: %i[create destroy]
   end
 
