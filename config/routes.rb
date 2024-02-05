041b961fd81692982d7e6bc5640b431809e58aa0
Kadina1988 <kadina.aziev@mail.ru>
2024-02-05 18:42:26 +0300

    updated form

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :posts, shallow: true do
-    resources :comments, only: :new
     resources :likes, only: %i[create]
-    # resources :likes, only: :destroy, as: 'post_like'
   end
 
   resources :posts do
-    resources :likes, only: :destroy
     resources :comments, only: %i[create]
+    resources :likes, only: :destroy
   end
 
   root 'posts#index'
