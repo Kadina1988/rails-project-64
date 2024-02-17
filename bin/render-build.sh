6e97c71b48f7ebac15109f8473386299f3d7413e
Kadina1988 <kadina.aziev@mail.ru>
2024-02-17 20:04:05 +0300

    end

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:drop
-bundle exec rails db:migrate
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails db:drop
+# bundle exec rails db:migrate
 # bundle exec rails db:seed
 
