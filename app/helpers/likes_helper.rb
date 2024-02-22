e9c1729c8b76f2e42c747c7dc944973d265f46a6
Kadina1988 <kadina.aziev@mail.ru>
2024-02-22 18:57:43 +0300

    add form classes

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -4,9 +4,9 @@ module LikesHelper
   def managing_likes(post)
     like = PostLike.find_by(post_id: post.id, user_id: current_user.id)
     if post.likes.include?(like)
-      link_to 'Like', post_like_path(like.post, like), data: { turbo_method: :delete }
+      link_to '', post_like_path(like.post, like), data: { turbo_method: :delete }, class: 'bi bi-hand-thumbs-up-fill'
     else
-      link_to 'Like', post_likes_path(post), data: { turbo_method: :post }
+      link_to '', post_likes_path(post), data: { turbo_method: :post }, class: 'bi bi-hand-thumbs-up'
     end
   end
 end
