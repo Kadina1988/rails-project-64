7d67e062926e109f9cd212a945f4d259c0011740
Kadina1988 <kadina.aziev@mail.ru>
2024-03-20 19:29:13 +0300

    added internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    lang = params[:lang] || I18n.default_locale
+    I18n.with_locale(lang, &action)
+  end
+
+  def default_url_options(options = {})
+    { lang: I18n.locale }
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,17 +3,17 @@
 module ApplicationHelper
   def sign_in_helper
     if user_signed_in?
-      link_to 'New post', new_post_path, class: 'btn bnt-secondary'
+      link_to t('new_post'), new_post_path, class: 'btn bnt-secondary'
     else
-      link_to 'Sign in', new_user_session_path, class: 'btn bnt-secondary'
+      link_to t('.sign_in'), new_user_session_path, class: 'btn bnt-secondary'
     end
   end
 
   def sign_out_helper
     if user_signed_in?
-      link_to 'Sign out', destroy_user_session_path, data: { turbo_method: :delete }, class: 'btn bnt-secondary'
+      link_to t('.sign_out'), destroy_user_session_path, data: { turbo_method: :delete }, class: 'btn bnt-secondary'
     else
-      link_to 'Sign up', new_user_registration_path, class: 'btn bnt-secondary'
+      link_to t('.sign_up'), new_user_registration_path, class: 'btn bnt-secondary'
     end
   end
 end
